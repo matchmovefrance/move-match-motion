5301bce670e393a3043b79d02aa6ea12d5dacca6
matchmovefrance <contact@matchmove.fr>
2025-06-06 15:50:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/apx/matchmove/', // Chemin de base absolu
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1000, // Augmente la limite d'avertissement
+  }
 }));
